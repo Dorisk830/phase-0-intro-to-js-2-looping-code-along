032f3a70311ee64bdcaba31b8c6a8632c9a83c4a
DorisK830 <doris.kiprop@student.moringaschool.com>
2023-09-28 05:53:07 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solutions in this file
+
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+const result=[];
+
+
+function writeCards(names, event) {
+  for (let i = 0; i < names.length; i++) {
+   // console.log(`Thank You ${names[i]} for the wonderful ${event} gift!`);
+    result.push(`Thank You, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return result;
+  debugger
+}
+
+const results=writeCards(names, "surprise")
+console.log(results)
+
+function countDown(countdown){
+  while (countdown>=0 ) {
+    console.log(countdown--);
+  }
+  
+}
+
+countDown(10);
+
+
